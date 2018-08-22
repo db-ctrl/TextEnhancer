1d4dc4b5e92b93002efd134fdfdb65380d808603
David Gay <djg9610@gmail.com>
2018-08-21 22:59:40 -0400

    Robust model saving. Logic changes. New parameters.
    - Robust sequential model naming with epoch loss improvement checkpoints.
    - Whole model is now saved instead of just weights. Allows for continuation of training from loaded model.
    - More parameter options to accommodate for new model saving system.

--- Network.py
+++ Network.py
@@ -1,26 +1,62 @@
 
-#LSTM RNN for generating text predictions.
+# LSTM RNN for generating text predictions.
 
 import numpy as np
-import pandas as pd
+import sys
 import os
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import LSTM
 from keras.utils import np_utils
+from keras.callbacks import ModelCheckpoint
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-################### Parameters #############################
+# Find next number in sequence for saving model.
+model_list = (os.listdir('./Models/'))
+
+biggest_num = 0
+
+for i in range(len(model_list) - 1):
+    x = model_list[i]
+    x = int(x[6:-3])
+    if x > biggest_num:
+        biggest_num = x
+
+model_num = biggest_num + 1
+
+################################################################
+
+##############
+# Parameters #
+##############
+
+# Path of text to train from.
+INPUT_TEXT_PATH = './Input/input.txt'
+# Path where model will output to / load from.
+MODEL_PATH_L = './Models/model ' + str(model_num - 1) + '.h5'
+MODEL_PATH_O = './Models/model ' + str(model_num) + '.h5'
+# Number of epochs to train for.
 EPOCHS = 10
-BATCH_SIZE = 300
+BATCH_SIZE = 150
+# Sequence starting point.
+# OUTPUT_START = 0
+OUTPUT_START = 0
+# Sequence length until prediction starts.
+# SEQUENCE_LENGTH = 120
 SEQUENCE_LENGTH = 120
-INPUT_TEXT_PATH = './input.txt'
-############################################################
+# Length of prediction.
+# PRED_LENGTH = 100
+PRED_LENGTH = 100
+# Flag for if loading model from file.
+LOAD_MODEL = True
+# Flag for if training model.
+TRAINING = True
+
+################################################################
 
 # Import text to train with and set to lowercase.
-
 print("Importing text...")
 
 input_text = open(INPUT_TEXT_PATH).read()
@@ -28,30 +64,34 @@ input_text = input_text.lower()
 
 # Map all unique characters to a number.
 chars = sorted(list(set(input_text)))
-
-n_to_char = {n:char for n, char in enumerate(chars)}
-char_to_n = {char:n for n, char in enumerate(chars)}
+n_to_char = {n: char for n, char in enumerate(chars)}
+char_to_n = {char: n for n, char in enumerate(chars)}
 
 X = []
 Y = []
 length = len(input_text)
 seq_length = SEQUENCE_LENGTH
 
+# Break text up into sequences.
 for i in range(0, length - seq_length, 1):
-	sequence = input_text[i:i + seq_length]
-	label= input_text[i + seq_length]
-	X.append([char_to_n[char] for char in sequence])
-	Y.append(char_to_n[label])
+    sequence = input_text[i:i + seq_length]
+    label = input_text[i + seq_length]
+    X.append([char_to_n[char] for char in sequence])
+    Y.append(char_to_n[label])
 
+# Reshape X, scale X by character amount to improve training speed,
+# and one-hot encode Y.
 X_modified = np.reshape(X, (len(X), seq_length, 1))
 X_modified = X_modified / float(len(chars))
 Y_modified = np_utils.to_categorical(Y)
 
 print("Done.")
-print("Unique Characters:",len(chars))
+print("Unique Characters:", len(chars))
 
+# Design model for training.
 model = Sequential()
-model.add(LSTM(250, input_shape=(X_modified.shape[1], X_modified.shape[2]), return_sequences=True))
+model.add(LSTM(250, input_shape=(X_modified.shape[1], X_modified.shape[2]),
+          return_sequences=True))
 model.add(Dropout(0.3))
 model.add(LSTM(250, return_sequences=True))
 model.add(Dropout(0.2))
@@ -61,26 +101,36 @@ model.add(Dense(Y_modified.shape[1], activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-if not os.path.isfile('./model.h5'):
+if LOAD_MODEL:
 
-	model.fit(X_modified, Y_modified, epochs=EPOCHS, batch_size=BATCH_SIZE)
+    if model_num - 1 == 0:
 
-	model.save_weights('./model.h5')
-	print("Model saved.")
+        sys.exit("No model file to load.")
 
-else:
-	
-	model.load_weights('./model.h5')
-	print("Model loaded from file.")
+    else:
+
+        model = load_model(MODEL_PATH_L)
+        print("Model loaded from", MODEL_PATH_L)
+
+if TRAINING:
+
+    checkpoint = ModelCheckpoint(MODEL_PATH_O, monitor='loss', verbose=1,
+                                 save_best_only=True, mode='min')
+    cb_list = [checkpoint]
+    model.fit(X_modified, Y_modified, epochs=EPOCHS, batch_size=BATCH_SIZE,
+              callbacks=cb_list)
+
+if not TRAINING and not LOAD_MODEL:
+
+    sys.exit("Can't generate text without training or loading model.")
 
 print("Generating text...")
 
-# string_mapped = X[np.random.randint(len(X))]
-string_mapped = X[0]
+string_mapped = X[OUTPUT_START]
 full_string = [n_to_char[value] for value in string_mapped]
 
-for i in range(seq_length):
-    x = np.reshape(string_mapped,(1,len(string_mapped), 1))
+for i in range(PRED_LENGTH):
+    x = np.reshape(string_mapped, (1, len(string_mapped), 1))
     x = x / float(len(chars))
 
     pred_index = np.argmax(model.predict(x, verbose=0))
@@ -90,11 +140,11 @@ for i in range(seq_length):
     string_mapped.append(pred_index)
     string_mapped = string_mapped[1:len(string_mapped)]
 
-text=""
+text = ""
 
 for char in full_string:
-    text = text+char
+    text = text + char
 
-print("-"*120)
+print("-" * 100)
 print(text)
-print("-"*120)
+print("-" * 100)
