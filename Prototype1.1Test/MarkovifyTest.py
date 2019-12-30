fab1d79ac4f496555edbda8abd8fcf4e56a6d76f
David Bailey <davidcharlesbailey@gmail.com>
2019-12-30 23:15:18 +0000

    Working on lambda function to sort best sentences

--- Prototype1.1Test/MarkovifyTest.py
+++ Prototype1.1Test/MarkovifyTest.py
@@ -1,16 +1,21 @@
 import markovify
-
+from Prototype1Test import Predict
 # Get raw text as string.
 with open("TestSentences.txt") as f:
     text = f.read()
 
 # Build the model.
 text_model = markovify.Text(text)
+# generate 100 and choose the 5th. 1st line boring, hgh line it starts not making sense, so 5 seemed good.
+
+model = markovify.Text(text) # get your model as normal
+Predict.getSent(model, 100, 4)#[5]
+
 
 # Print five randomly-generated sentences
-for i in range(5):
-    print(text_model.make_sentence())
-
+#for i in range(5):
+#    print(text_model.make_sentence())
+#
 # Print three randomly-generated sentences of no more than 280 characters
-for i in range(3):
-    print(text_model.make_short_sentence(280))
+#for i in range(3):
+#    print(text_model.make_short_sentence(280))
---
+++ Prototype1Test/Predict.py
@@ -0,0 +1,40 @@
+def getSent(model, iters, minLength=1):
+    sentences = {}
+    for i in range(iters):
+        modelGen = model.chain.gen()
+        prevPrevWord = "___BEGIN__"
+        prevWord = modelGen.__next__()
+        madeSentence = prevWord + " "
+
+        totalScore = 0
+        numWords = 1
+        for curWord in modelGen:
+            madeSentence += curWord + " "
+            numWords += 1
+            totalScore += model.chain.model[(prevPrevWord, prevWord)][curWord]
+            prevPrevWord = prevWord
+            prevWord = curWord
+
+        madeSentence = madeSentence.strip()
+        if numWords == 0: continue
+
+        # These commonly appear if you are using reddit data so I ignore them
+        if "am a bot" in madeSentence.lower(): continue
+        if "questions or concerns" in madeSentence.lower(): continue
+        if "contact the moderators" in madeSentence.lower(): continue
+        if "this action was performed" in madeSentence.lower(): continue
+        if numWords < minLength: continue
+        if madeSentence in sentences: continue
+
+        totalScore += model.chain.model[(prevPrevWord, prevWord)]["___END__"]
+
+        sentences[madeSentence] = totalScore / float(numWords)
+
+    # Get the sentences as (sentence, score) pairs
+    sentences = sentences.items()
+
+    # Sort them so the sentences with the highest score appear first
+    # TODO: fix the below function to be compatible with the rules of pythonn3.0
+    sentences.sort(key=lambda x: -x[1])
+
+    return sentences
