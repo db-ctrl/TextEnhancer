76c3e0cef887513043ef54d7d3ced4fc54419541
David Bailey <davidcharlesbailey@gmail.com>
2020-01-15 15:36:53 +0000

    Modifying Translate Function to update from py2 -> py3

--- Prototype1/2/Main.py
+++ Prototype1/2/Main.py
@@ -1,9 +1,9 @@
 import spacy
 from textstat.textstat import textstatistics, easy_word_set, legacy_round
 
-from Prototype1Test import SpacyFuncs
+from Prototype1Test import ClusterTest
 # Get raw text as string.
 with open("TestSentences2.txt") as f:
     text = f.read()
 
-print(SpacyFuncs.avg_sentence_length(text))
+print(ClusterTest(text))
