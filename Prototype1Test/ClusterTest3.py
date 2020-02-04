8507dd83d5be8969e4d75546bdf990567bdcbdd0
David Bailey <davidcharlesbailey@gmail.com>
2020-02-04 19:46:26 +0000

    - Reinstalled python, TK box wasnt ticked ffs

--- Prototype1Test/ClusterTest3.py
+++ Prototype1Test/ClusterTest3.py
@@ -57,32 +57,3 @@ print("The Sentence:", sentence1, prediction)
 Y = vectorizer.transform([sentence2])
 prediction = model.predict(Y)
 print("The Sentence:", sentence2, prediction)
-
-import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.manifold import TSNE
-
-train = ["is this good?", "this is bad", "some other text here", "i am hero", "blue jeans", "red carpet", "red dog",
-     "blue sweater", "red hat", "kitty blue"]
-
-vect = TfidfVectorizer()
-X = vect.fit_transform(train)
-clf = KMeans(n_clusters=3)
-data = clf.fit(X)
-centroids = clf.cluster_centers_
-
-tsne_init = 'pca'  # could also be 'random'
-tsne_perplexity = 20.0
-tsne_early_exaggeration = 4.0
-tsne_learning_rate = 1000
-random_state = 1
-model = TSNE(n_components=2, random_state=random_state, init=tsne_init, perplexity=tsne_perplexity,
-         early_exaggeration=tsne_early_exaggeration, learning_rate=tsne_learning_rate)
-
-transformed_centroids = model.fit_transform(centroids)
-
-print (transformed_centroids)
-
-plt.scatter(transformed_centroids[:, 0], transformed_centroids[:, 1], marker='x')
-plt.show()
