f988bc930773a46cbeacac5d19f13ed4ba5fdf37
David Bailey <davidcharlesbailey@gmail.com>
2020-02-05 17:02:29 +0000

    Cleaned up new cluster test (4)

--- Prototypes/Clusters/ClusterTest4.py
+++ Prototypes/Clusters/ClusterTest4.py
@@ -1,10 +1,6 @@
-
-import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.manifold import TSNE
 
-from Prototype1Test import SpacyFuncs
 # Get raw text as string.
 with open("blank.txt") as f:
     rawtext = f.read()
@@ -36,6 +32,8 @@ print("Prediction")
 sentence1 = "Mr dursley is a nasty man"
 sentence2 = "harry potter stole my wife."
 
+# TODO : investigate how the dotPredict module works (pursuing) sentence labelling.
+
 Y = vectorizer.transform([sentence1])
 prediction = model.predict(Y)
 print("The Sentence:", sentence1, prediction)
@@ -44,25 +42,3 @@ Y = vectorizer.transform([sentence2])
 prediction = model.predict(Y)
 print("The Sentence:", sentence2, prediction)
 
-train = documents
-
-vect = TfidfVectorizer()
-X = vect.fit_transform(train)
-
-data = model.fit(X)
-centroids = data.cluster_centers_
-
-tsne_init = 'pca'  # could also be 'random'
-tsne_perplexity = 20.0
-tsne_early_exaggeration = 4.0
-tsne_learning_rate = 1000
-random_state = 1
-model = TSNE(n_components=2, random_state=random_state, init=tsne_init, perplexity=tsne_perplexity,
-         early_exaggeration=tsne_early_exaggeration, learning_rate=tsne_learning_rate)
-
-transformed_centroids = model.fit_transform(centroids)
-
-print (transformed_centroids)
-
-plt.scatter(transformed_centroids[:, 0], transformed_centroids[:, 1], marker='x')
-plt.show()
