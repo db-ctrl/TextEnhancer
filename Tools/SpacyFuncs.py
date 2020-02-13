8d9992b8f32bc4b4ae43fc4734ba3eba349ffba4
David Bailey <davidcharlesbailey@gmail.com>
2020-02-13 12:00:49 +0000

    - Further fix on replacing special characters

--- Tools/SpacyFuncs.py
+++ Tools/SpacyFuncs.py
@@ -13,6 +13,8 @@ def break_sentences(text):
     # removing all newlines
     # TODO: fix "'", "'s Screams..." sentences
     text = text.replace('\n', ' ')
+    text = text.replace('‘', '"')
+    text = text.replace('’', '"')
 
     # Configure document nlp
     nlp = en_core_web_sm.load()
@@ -20,13 +22,12 @@ def break_sentences(text):
     sentences = []
 
     for sent in doc.sents:
-
+        # if sent
         single = sent.string.strip()
-
         # TODO: Make catch for last  comma at end of list
-
         sentences.append("\"" + single + "\",")
 
+    # sentences[:-1] = sent.string.strip(',')
     return sentences
 
 # Returns Number of Words in the text
