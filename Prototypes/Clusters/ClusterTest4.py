f9f471eae83a50f214bd026140ed68bbd046ccfb
David Bailey <davidcharlesbailey@gmail.com>
2020-02-24 21:41:09 +0000

    Proress on cluster mapping rebuild

--- Prototypes/Clusters/ClusterTest4.py
+++ Prototypes/Clusters/ClusterTest4.py
@@ -4,6 +4,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 INPUT_TEXT_PATH = '/Users/david/PycharmProjects/LSTM-Text-Generator/MainModules/MainPackage/Out_Docs.txt'
 
+sentence1 = "Harry and the Weasleys spent a happy afternoon having a furious snowball fight on the grounds."
+
 # Get raw text as string.
 with open(INPUT_TEXT_PATH) as f:
     rawtext = f.read()
@@ -11,9 +13,6 @@ with open(INPUT_TEXT_PATH) as f:
 # Convert sentence string into iterable list
 
 # TODO: Ensure below is pulling through full sentences
-
-#Splits via comma
-# sentList = rawtext.split(",")
 
 sentList = rawtext.split(",")
 
@@ -29,39 +28,32 @@ model.fit(X)
 print("Top terms per cluster:")
 order_centroids = model.cluster_centers_.argsort()[:, ::-1]
 terms = vectorizer.get_feature_names()
-for i in range(true_k):
-    print("Cluster %d:" % i),
-    for ind in order_centroids[i, :15]:
-        print(' %s' % terms[ind]),
-    print
-
-print("\n")
-print("Cluster to word mapping: ")
-
-# TODO: Make catch for blank spaces at end of sentences
-
-sentence1 = "lot ti sores and tp suederily as the wall and the cat hn a sery off houn the bat had been hare a siiht"
 
 wordList = sentence1.split(" ")
 out_str = ''
-# TODO : Make nested for multiple sentences
 
-# TODO: investigate where 'Terms' are used in relation to clusters (see what attributes are reduced , e.g. and)
+for i in range(true_k):
+    print("Cluster %d:" % i),
+    for ind in order_centroids[i, :15]:
+        print(' %s' % terms[ind])
+
+#TODO: Make for loop only print ONCE per cluster hit
+
+for w in wordList:
+    w = w.lower()
+    if w in terms[ind]:
+        out_str += str(w)
+        out_str += str(" ")
+        out_str += ("%d:" % i)
+print("\n")
+print("Cluster to word mapping: ")
+print(out_str)
 
 
-for i in wordList:
-    Y = vectorizer.transform([i])
-    if i not in terms:
-        out_str += str(i)
-        out_str += "[N/A]"
-        out_str += " "
-    else:
-        prediction = model.predict(Y)
-        out_str += str(i)
-        out_str += str(prediction)
-        out_str += " "
-
-print(out_str, entropy([10/10, 1/10], base=2))
+#else:
+#out_str += str(w)
+#out_str += "[N/A]"
+#out_str += " "
 
 
 
