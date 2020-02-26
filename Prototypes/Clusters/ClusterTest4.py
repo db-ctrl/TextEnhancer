d49c1cccea4ef08870656c6563140621ce10d433
David Bailey <davidcharlesbailey@gmail.com>
2020-02-26 17:42:30 +0000

    Added counter for total hits in all clusters

--- Prototypes/Clusters/ClusterTest4.py
+++ Prototypes/Clusters/ClusterTest4.py
@@ -1,6 +1,8 @@
 from sklearn.cluster import KMeans
+from sklearn.neighbors import NearestCentroid
 from scipy.stats import entropy
 from sklearn.feature_extraction.text import TfidfVectorizer
+from collections import Counter
 
 INPUT_TEXT_PATH = '/Users/david/PycharmProjects/LSTM-Text-Generator/MainModules/MainPackage/Out_Docs.txt'
 
@@ -15,13 +17,17 @@ with open(INPUT_TEXT_PATH) as f:
 # TODO: Ensure below is pulling through full sentences
 
 sentList = rawtext.split(",")
-
 documents = sentList
+
+# Counter for words in clusters
+wordsInClus = 0
 
 vectorizer = TfidfVectorizer(stop_words='english')
 X = vectorizer.fit_transform(documents)
 
-true_k = 300
+
+
+true_k = 250
 model = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1)
 model.fit(X)
 
@@ -31,29 +37,32 @@ terms = vectorizer.get_feature_names()
 
 wordList = sentence1.split(" ")
 out_str = ''
-
+hitList = []
+# TODO Calculate how many times terms occour in each cluster, fill hitlist and then count instances
 for i in range(true_k):
     print("Cluster %d:" % i),
-    for ind in order_centroids[i, :15]:
+# Print x amount of words from each cluster
+    for ind in order_centroids[i, :10]:
+        hitList.insert(ind, terms[ind])
         print(' %s' % terms[ind])
+
+# check if a specific word is in a cluster
     if terms[ind] in wordList:
         out_str += str(terms[ind])
         out_str += str(" ")
-        out_str += ("Cluster %d:" % i)
+        out_str += ("[" + "%d" % i + "] ")
+        wordsInClus += 1
 
-#TODO: Make for loop only print ONCE per cluster hit
+print("\n" + "Test sentence: " + "\n" + sentence1 + "\n")
 
-print("\n")
 print("Cluster to word mapping: ")
+
 print(out_str)
+print(wordsInClus)
 
+totalHits = dict(Counter(hitList))
 
-#else:
-#out_str += str(w)
-#out_str += "[N/A]"
-#out_str += " "
-
-
+print(totalHits)
 
 
 
