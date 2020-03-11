90f2578e5a3e6c712476f3ebea746783a45bfe21
David Bailey <davidcharlesbailey@gmail.com>
2020-03-11 15:47:30 +0000

    updated add to sheet on lstm train

--- MainModules/TestRigs/TestRig2.py
+++ MainModules/TestRigs/TestRig2.py
@@ -1,5 +1,6 @@
-from Tools import SpacyFuncsTest
 
-with open("p.txt") as f:
-    text = f.read()
-print(SpacyFuncsTest.flesch_reading_ease(text))
+list = ['the', 'end', 'of', 'the', 'few', 'door', 'the', 'end', 'of', 'the', 'aside', 'with', 'his', 'free', 'hand.', 'the', 'kitchen', 'door', 'opened']
+
+list2 = ['the', 'end', 'of', 'the', 'few', 'door', 'the', 'end', 'of', 'the', 'aside', 'with', 'his', 'free', 'hand.', 'the', 'kitchen', 'door', 'opened']
+list2.pop(0)
+print(' '.join(map(str, list)) + ' '.join(map(str, list2)))
